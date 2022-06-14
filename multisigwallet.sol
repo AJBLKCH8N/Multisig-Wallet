51c610c123a7f6ac1a19ca1895b4400fe7682feb
Tsuchinoko69 <anupatel69@gmail.com>
2022-06-15 07:32:38 +0800

    added multisig wallet contract

---
+++ multisigwallet.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier:UNLICENSED
+pragma solidity 0.8.14;
+
+contract multisigwallet {
+
+function deposit () public {
+
+}
+
+function transfer () public {
+
+}
+
+function getBalance () public {
+
+}
+
+}
