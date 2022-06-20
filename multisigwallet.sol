4018b1e0c08570b8dff1446953fe9e114706927b
Tsuchinoko69 <anupatel69@gmail.com>
2022-06-21 05:58:42 +0800

    Update 21st

--- multisigwallet.sol
+++ multisigwallet.sol
@@ -1,18 +1,64 @@
 //SPDX-License-Identifier:UNLICENSED
-pragma solidity 0.8.14;
+pragma solidity 0.8.15;
+pragma abicoder v2;
 
 contract multisigwallet {
 
-function deposit () public {
+address[] public owners;
+uint limit;
+
+struct Transfer{
+    address transferTo;
+    uint transferAmount;
+    uint8 numberApprovals;
+}
+
+Transfer[] transferRequests;
+
+mapping(address => mapping(uint => bool)) approvals;
+//double mapping - set address to point to a mapping where you input ID of address, and it returns T/F
+//mapping[msg.sender][transferID] => True/False
+
+function addOwner(address Owner) public{
 
 }
 
-function transfer () public {
+function removeOwner(address Owner) public{
 
 }
 
-function getBalance () public {
+
+function deposit () public payable returns(uint) {
+    address(this).balance += msg.value;
+    //deposit amount to the contract
 
 }
+
+function withdrawl () public {
+
+}
+
+function getBalance() public view returns(uint) {
+    return address(this).balance;
+
+}
+
+
+function approval(int nApprovals) public view returns() {
+    if (nApprovals == approvals){
+    }
+    else {
+    }
+
+//check if there are enough approvals
+}
+
+function transfer(address to, uint amount) public {
+
+
+
+
+}
+
 
 }
